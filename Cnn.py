1590dafc7e490f38155f609cd52556c2e3b92f42
Sentixxx <1091441107@qq.com>
2024-03-29 18:57:07 +0800

    rewrite FcNet and ConvNet

--- Cnn.py
+++ Cnn.py
@@ -26,12 +26,12 @@ import tensorflow_addons as tfa
 class DilatedConv(tf.keras.layers.Layer):
     def __init__(self, specs, keep_prob=1.0, **kwargs):
         super(DilatedConv, self).__init__(**kwargs)
-        self.conv_specs = specs 
+        self.specs = specs 
         self.keep_prob = keep_prob
         self.conv_layers = []
 
     def build(self, input_shape):
-        for i,(fun_name, w_size, rate, out_channel) in enumerate(self.conv_specs):
+        for i,(fun_name, w_size, rate, out_channel) in enumerate(self.specs):
             conv_layer = tf.keras.layers.Conv2D(
                 filters=out_channel,
                 kernel_size=w_size,
@@ -52,96 +52,67 @@ class DilatedConv(tf.keras.layers.Layer):
             x = tf.keras.activations.get(fun_name)(x)
         return x
 
-class ConvNet(object):
-    def __init__(self, specs, inputs, training, deconv=False, keep_prob=1.0):
-        self.conv_layers = []
+class ConvNet(tf.keras.layers.Layer):
+    def __init__(self, specs,keep_prob=1.0,**kwargs):
+        super(ConvNet, self).__init__(**kwargs)
+        self.specs = specs
         self.keep_prob = keep_prob
-        outputs = inputs
-        for i, (fun_name, w_size, strides, out_channel) in enumerate(specs):
-            self.conv_layers.append(outputs)
-            if deconv == False:
-                with tf.compat.v1.variable_scope('conv%d' % i):
-                    outputs = self.build_conv_layer(outputs, w_size, out_channel, strides, fun_name, training=training)
+        self.conv_layers = []
+
+    def build(self,input_shape,deconv=False):
+        for i, (fun_name, w_size, strides, out_channel) in enumerate(self.specs):
+            if not self.deconv:
+                conv_layer = tf.keras.layers.Conv2D(
+                    filters=out_channel,
+                    kernel_size=w_size,
+                    strides=strides,
+                    padding='SAME',
+                    activation=None,
+                    use_bias=False,
+                    kernel_initializer='glorot_uniform'
+                )
             else:
-                with tf.compat.v1.variable_scope('deconv%d' % i):
-                    outputs = self.build_deconv_layer(outputs, w_size, out_channel, strides, fun_name, training=training)
-        self.conv_layers.append(outputs)
+                conv_layer = tf.keras.layers.Conv2DTranspose(
+                    filters=out_channel,
+                    kernel_size=w_size,
+                    strides=strides,
+                    padding='SAME',
+                    activation=None,
+                    use_bias=False,
+                    kernel_initializer='glorot_uniform'
+                )
+        bn_layer = tf.keras.layers.BatchNormalization()
+        self.conv_layers.append((conv_layer, bn_layer, fun_name))
 
-    def xavier_init(self, size):
-        in_dim = size[0]
-        xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
-        return tf.random.normal(shape=size, stddev=xavier_stddev)
 
-    def get_filter(self, name, shape):
-        return tf.compat.v1.get_variable(name, dtype=tf.float32, initializer=self.xavier_init(shape),use_resource=True)
+    def call(self, inputs, training=None):
+        x = inputs
+        for conv_layer, bn_layer, fun_name in self.conv_layers:
+            x = conv_layer(x, training=training)
+            x = bn_layer(x, training=training)
+            x = tf.keras.activations.get(fun_name)(x)
+        return x
 
-    def select_act_func(self, actfun):
-        if actfun == 'tanh':
-            return tf.nn.tanh
-        elif actfun == 'sigmoid':
-            return tf.sigmoid
-        elif actfun == 'relu':
-            return tf.nn.relu
-        else:
-            return lambda x: x
-
-    def build_conv_layer(self, inputs, w_size, out_channel, strides=[1, 2, 2, 1], actfun='relu', training=True):
-        batch_size, height, width, in_channel = inputs.get_shape().as_list()
-        w = self.get_filter('filter', [w_size[0], w_size[1], in_channel, out_channel])
-        f = self.select_act_func(actfun)
-        conv = tf.nn.conv2d(inputs, w, strides=strides, padding='SAME')
-        InstanceNormalization()(conv)
-        out = f(conv)
-        return out
-
-    def build_deconv_layer(self, inputs, w_size, out_channel, strides=[1, 2, 2, 1], actfun='relu', training=True):
-        batch_size, height, width, in_channel = inputs.get_shape().as_list()
-        w = self.get_filter('filter', [w_size[0], w_size[1], out_channel, in_channel])
-        if strides[1] == 1:
-            deconv_shape = [batch_size, height, width, out_channel]
-        else:
-            deconv_shape = [batch_size, height * 2, width * 2, out_channel]
-        f = self.select_act_func(actfun)
-        conv = tf.nn.conv2d_transpose(inputs, w, deconv_shape, strides=strides, padding='SAME')
-        InstanceNormalization()(conv)
-        out = f(conv)
-        return out
-
-class FcNet(object):
-    def __init__(self, specs, inputs):
+class FcNet(tf.keras.layers.Layer):
+    def __init__(self, specs, **kwargs):
+        super(FcNet, self).__init__(**kwargs)
+        self.specs = specs
         self.fc_layers = []
-        outputs = inputs
-        for i, (fun_name, in_size, out_size, scope) in enumerate(specs):
-            self.fc_layers.append(outputs)
-            with tf.compat.v1.variable_scope(scope):
-                outputs = self.build_fc_layer(x=outputs, input_size=in_size, output_size=out_size, actfun=fun_name)
-        self.fc_layers.append(outputs)
+    
+    def build(self, input_shape):
+        for i, (fun_name, out_channel) in enumerate(self.specs):
+            fc_layer = tf.keras.layers.Dense(
+                units=out_channel,
+                activation=None,
+                use_bias=True,
+                kernel_initializer='glorot_uniform'
+            )
+            self.fc_layers.append((fc_layer, fun_name))
 
-    def xavier_init(self, size):
-        in_dim = size[0]
-        xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
-        return tf.random.normal(shape=size, stddev=xavier_stddev)
 
-    def select_act_func(self, actfun):
-        if actfun == 'tanh':
-            return tf.nn.tanh
-        elif actfun == 'sigmoid':
-            return tf.sigmoid
-        elif actfun == 'relu':
-            return tf.nn.relu
-        else:
-            return lambda x: x
-
-    def build_fc_layer(self, x, input_size, output_size, actfun, scope=None, use_bias=True):
-        with tf.compat.v1.variable_scope(scope or 'linear'):
-            w = tf.compat.v1.get_variable(name='fc_w', dtype=tf.float32, initializer=self.xavier_init([input_size, output_size]),use_resource=True)
-            if use_bias:
-                b = tf.compat.v1.get_variable('fc_b', [output_size], tf.float32, initializer=tf.compat.v1.constant_initializer(0.0),use_resource=True)
-                temp = tf.matmul(x, w) + b
-            else:
-                temp = tf.matmul(x, w)
-            if actfun == 'no':
-                return temp
-            else:
-                f = self.select_act_func(actfun)
-                return f(temp)
+    def call(self, inputs, training=None):
+        x = inputs
+        for fc_layer, fun_name in self.fc_layers:
+            x = fc_layer(x, training=training)
+            x = tf.keras.activations.get(fun_name)(x)
+        return x
